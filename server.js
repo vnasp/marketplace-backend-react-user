d8d67ce7af858c94e509a7ebb37c920133a1990d
Valentina <valentinamr@gmail.com>
2024-03-01 10:32:49 -0300

    Add Swagger to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { logger } from 'logger-express';
+import swagger from './config/swagger/swagger.js';
 import loginRoutes from './config/routes/loginRoutes.js';
 import usersRoutes from './config/routes/usersRoutes.js';
 import {errorController} from './src/api/v1/controllers/errorController.js';
@@ -8,11 +10,16 @@ import 'dotenv/config';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+swagger(app)
 app.use(express.json());
 app.use(cors());
-
+app.use(logger());
 app.use("/", loginRoutes);
 app.use("/", usersRoutes);
 app.use("*", errorController.error404);
 
-app.listen(PORT, console.log(`Server running on port ${PORT}...`));
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`);
+});
