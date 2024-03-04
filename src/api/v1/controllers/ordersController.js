0a841797371589cc1bb4bbca498eebf5732593e5
Valentina <valentinamr@gmail.com>
2024-03-04 16:19:52 -0300

    Add create orders

--- src/api/v1/controllers/ordersController.js
+++ src/api/v1/controllers/ordersController.js
@@ -1,5 +1,43 @@
-const createOrder = async(req, res) => {};
+import { orderModel } from "../models/orderModel.js";
 
-const getOrdersByAuthUser = async(req,res) =>{};
+const createOrder = async (req, res) => {
+  try {
+    const id_user = req.auth.id_user;
+    const orderData = { ...req.body, id_user }
+    const order = await orderModel.createOrderDB(orderData);
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const getOrdersByAuthUser = async (req, res) => {
+  try {
+    const id_user = req.auth.id_user;
+    const orders = await productModel.getUserOrders(id_user);
+    if (!orders) {
+      return res.status(404).json({ message: 'Órden no encontrada' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const ordersController = { createOrder, getOrdersByAuthUser };
+
+/*
+{
+  "total_price": 10000,
+  "products": [
+    {
+      "id_product": 1,
+      "product_quantity": 2
+    },
+    {
+      "id_product": 2,
+      "product_quantity": 3
+    }
+  ]
+}
+*/
--- src/api/v1/models/orderModel.js
+++ src/api/v1/models/orderModel.js
@@ -1,28 +1,54 @@
-/*
-::createOrder()
+import pool from "../../../../config/database/connection.js"
 
-request
-{
-  "order": {
-    "id_order": 1234,
-    "id_user": 1,
-    "total_price": 30000,
-    "id_product": 1,
-    "product_quantity": 1,
-    "purchase_date": "2024-02-14T18:00:00.000Z"
+
+// Creación de la orden y luego crear el detalle
+const createOrderDB = async (orderData) => {
+  const { id_user, total_price, products } = orderData
+
+  // Validación de total_price
+  if (total_price <= 0) {
+    throw new Error('El monto total debe ser mayor que cero.')
+  }
+
+  // Validación de products
+  if (!Array.isArray(products) || products.length === 0) {
+    throw new Error('La orden debe contener al menos un producto.')
+  }
+  products.forEach(product => {
+    if (product.product_quantity <= 0) {
+      throw new Error('La cantidad de cada producto debe ser mayor que cero.')
+    }
+  })
+
+  try {
+    await pool.query('BEGIN')
+    // Creamos la orden y obtenemos el id_order generado
+    const createOrder = await pool.query('INSERT INTO orders (id_user, total_price) VALUES ($1, $2) RETURNING id_order, purchase_date', [id_user, total_price]);
+    const { id_order, purchase_date } = createOrder.rows[0];
+
+    // Con el id_order obtenido, se inserta cada producto en order_details
+    for (const product of products) {
+      const { id_product, product_quantity } = product
+      await pool.query('INSERT INTO orders_details (id_order, id_product, product_quantity) VALUES ($1, $2, $3)', [id_order, id_product, product_quantity])
+    }
+    await pool.query('COMMIT')
+    return { id_order, id_user, total_price, products, purchase_date }
+  } catch (error) {
+    await pool.query('ROLLBACK') // Si no se crean los detalles, que deshaga todo
+    throw error
   }
 }
 
-response
-{
-  "id_order": 1234,
-  "id_user": 1,
-  "total_price": 30000,
-  "id_product": 1,
-  "product_quantity": 1,
-  "purchase_date": "2024-02-14T18:00:00.000Z"
+
+
+
+const getUserOrders = async (id) => {
+  const { rows } = await pool.query('SELECT * FROM orders WHERE id_user = $1', [id])
+  return rows[0]
 }
-*/
+
+export const orderModel = { createOrderDB, getUserOrders }
+
 
 /*
 ::getOrder()
