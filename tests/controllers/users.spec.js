11b9547035cae672ba6b204b915fc56f26aedf7a
Benjamín Segura Hernández <benjamin.segura.hernandez@gmail.com>
2024-03-04 19:28:01 -0300

    add test controllers + fix users tests

--- tests/controllers/users.spec.js
+++ tests/controllers/users.spec.js
@@ -1,30 +1,30 @@
 import request from "supertest";
-import app from "../server";
-import { credentials_valid, credentials_invalid, generateToken } from "./utils/data.js";
+import app from "../../server.js";
+import { credentials_valid, credentials_invalid, generateToken } from "../utils/data.js";
 
 describe("Mi Market Latino", () => {
-    it("GET /users : returns code 200 and array", async () => {
+    it("GET /users : returns code 200 and object", async () => {
         const response = await request(app)
-            .get("/users")
+            .get(`/api/v1/users/${credentials_valid.id_user}`)
             .set("Authorization", `Bearer: ${generateToken()}`)
             .send();
         
         expect(response.statusCode).toBe(200);
-        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body).toBeInstanceOf(Object);
     });
 
-    it("POST /login with valid credentials : returns object", async () => {
+    it("POST /login with valid credentials : returns code 200 and object", async () => {
         const response = await request(app)
-            .post("/login")
+            .post("/api/v1/login")
             .send(credentials_valid);
         
-        expect(response.statusCode).toBe(201);
+        expect(response.statusCode).toBe(200);
         expect(response.body).toBeInstanceOf(Object);
     });
 
     it("POST /login with invalid credentials : returns code 400", async () => {
         const response = await request(app)
-            .post("/login")
+            .post("/api/v1/login")
             .send(credentials_invalid);
         
         expect(response.statusCode).toBe(400);
