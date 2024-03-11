da90f0046f50dd3a1e34ace17715e8f4b44f7aaf
Benjamín Segura Hernández <benjamin.segura.hernandez@gmail.com>
2024-03-11 17:43:59 -0300

    fix favorites + tests coverage 60%

--- src/api/v1/controllers/favoritesController.js
+++ src/api/v1/controllers/favoritesController.js
@@ -8,7 +8,8 @@ const addFavorites = async (req, res) => {
         await favoriteModel.addFavorite(id_user, id_product);
         return res.status(201).json({ id_user, id_product });
     } catch (error) {
-        throw new Error("Error adding favorite: " + error.message);
+        res.locals.statusText = { error: `${error.message}` };
+        return res.status(500).json(res.locals.statusText);
     }
 };
 
@@ -17,31 +18,10 @@ const getFavoritesByUser = async (req, res) => {
     try {
         const { id_user } = req.auth;
         const favorites = await favoriteModel.getFavoriteByUser(id_user);
-        console.log("favoritos recuperados: ", favorites);
         return res.status(200).json(favorites);
     } catch (error) {
-        throw new Error("Error adding favorite: " + error.message);
-    }
-};
-
-// method: PUT
-const updateFavorites = async (req, res) => {
-    try {
-        const { id_user } = req.auth;
-        const { id_product } = req.body;
-        const isFavorite = await favoriteModel.existingFavorite(id_user);
-        let message;
-        if (isFavorite) {
-            await favoriteModel.removeFavorite(id_user, id_product);
-            message = "Favorite removed successfully";
-            return res.status(200).json({ id_user, id_product, message });
-        } else {
-            await favoriteModel.addFavorite(id_user, id_product);
-            message = "Favorite added successfully";
-            return res.status(201).json({ id_user, id_product, message });
-        }
-    } catch (error) {
-        throw new Error("Error updating favorite: " + error.message);
+        res.locals.statusText = { error: `${error.message}` };
+        return res.status(500).json(res.locals.statusText);
     }
 };
 
@@ -52,16 +32,16 @@ const removeFavorites = async (req, res) => {
         const { id_product } = req.body;
         await favoriteModel.removeFavorite(id_user, id_product);
         return res
-            .status(200)
+            .status(204)
             .json({ message: "Favorite removed successfully" });
     } catch (error) {
-        throw new Error("Error removing favorite: " + error.message);
+        res.locals.statusText = { error: `${error.message}` };
+        return res.status(500).json(res.locals.statusText);
     }
 };
 
 export const favoritesController = {
     addFavorites,
     getFavoritesByUser,
-    updateFavorites,
     removeFavorites,
 };
